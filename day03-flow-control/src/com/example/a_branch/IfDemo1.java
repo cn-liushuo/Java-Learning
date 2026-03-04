4b3aa30f0091a2ddb20abba865910a701d2e80a1
sure <liushuo_fe@163.com>
2026-03-04 10:24:03 +0800

    feat(day02): IfDemo1 代码格式化

--- day03-flow-control/src/com/example/a_branch/IfDemo1.java
+++ day03-flow-control/src/com/example/a_branch/IfDemo1.java
@@ -39,13 +39,13 @@ public class IfDemo1 {
         int score = sc.nextInt();
         if (score >= 90 && score <= 100) {
             System.out.println("A+");
-        } else if (score >= 80 && score <90) {
+        } else if (score >= 80 && score < 90) {
             System.out.println("A");
         } else if (score >= 70 && score < 80) {
             System.out.println("B");
         } else if (score >= 60 && score < 70) {
             System.out.println("C");
-        } else if (score >0 && score < 60) {
+        } else if (score > 0 && score < 60) {
             System.out.println("D");
         } else {
             System.out.println("分数输入有误！");
