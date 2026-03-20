02f3c585ec3db8e32cbe187d8de6506c8a0c7764
Sure <liushuo_fe@163.com>
2026-03-20 09:56:37 +0800

    feat(oop): static 修饰成员变量

---
+++ day05-oop/src/com/example/f_staticfield/B_Test.java
@@ -0,0 +1,12 @@
+package com.example.f_staticfield;
+
+public class B_Test {
+    public static void main(String[] args) {
+        // 目标：了解静态变量的应用。
+        new User();
+        new User();
+        new User();
+        new User();
+        System.out.println(User.count);
+    }
+}
