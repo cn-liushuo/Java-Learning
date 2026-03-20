995e97f2c2e410b8063dfce1d46b13b192a2956c
Sure <liushuo_fe@163.com>
2026-03-20 13:56:44 +0800

    feat(oop高级): 认识继承

---
+++ day06-oop/src/com/example/a_extends/Consultant.java
@@ -0,0 +1,14 @@
+package com.example.a_extends;
+
+// 咨询师类  子类
+public class Consultant extends People {
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
---
+++ day06-oop/src/com/example/a_extends/Teacher.java
@@ -0,0 +1,14 @@
+package com.example.a_extends;
+
+// 子类
+public class Teacher extends People {
+    private String skill; // 技术
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+}
