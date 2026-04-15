b1c6ccab49fcde12fe04c0257ea781c4afdbf147
ls <liushuo_fe@163.com>
2026-04-15 10:38:19 +0800

    feat(面向对象高级): final 关键字

---
+++ day07-oop/src/com/example/a_final/A_Final.java
@@ -0,0 +1,14 @@
+package com.example.a_final;
+
+public class A_Final {
+    public static void main(String[] args) {
+        // 目标：认识关键字 final 的作用
+    }
+}
+
+// 1、final 可以修饰类，类不能被继承。
+final class A {
+}
+
+//class B extends A {
+//}
