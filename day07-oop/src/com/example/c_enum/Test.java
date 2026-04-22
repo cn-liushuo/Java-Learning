bf79acd423304063131ddd834bd64d780e26a4c6
ls <liushuo_fe@163.com>
2026-04-22 14:41:53 +0800

    feat: 枚举类

--- day07-oop/src/com/example/c_enum/Test.java
+++ day07-oop/src/com/example/c_enum/Test.java
@@ -5,5 +5,14 @@ public class Test {
         // 目标：认识枚举类，搞清楚其本质特点
         A a1 = A.X;
         System.out.println(a1);
+
+        A a2 = A.Y;
+        System.out.println(a2);
+
+        System.out.println(a1.name()); // X
+        System.out.println(a2.name()); // Y
+        System.out.println(a1.ordinal()); // 索引 0
+        System.out.println(a2.ordinal()); // 索引 1
+
     }
 }
